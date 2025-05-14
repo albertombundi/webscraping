fc4463d7cf88e8d334829d8f8c53e7aa45005501
albertombundi <albertombundi@outlook.com>
2025-05-15 00:30:50 +0100

    Update do método de requisição e extrutura do código

--- scrape.js
+++ scrape.js
@@ -1,30 +1,31 @@
-const request = require('request');
-const cheerio = require('cheerio');
+const axios = require('axios'); // importa a biblioteca axios
+const cheerio = require('cheerio'); // importa a biblioteca cheerio 
 
-request('https://fakestoreapi.com', (error, response, html) => {
-    if(!error && response.statusCode == 200) {
-        const $  = cheerio.load(html);
-        
-        const siteHeading = $('.jumbotron');
+axios('https://fakestoreapi.com') // faz uma requisição para a URL com axios
+    .then(response => { // espera a resposta
+        if (response.status === 200) { // verifica se a resposta foi bem sucedida
+            const $ = cheerio.load(response.data); // carrega o HTML da resposta com cheerio
+            
+            const siteHeading = $('h1');  // seleciona o elemento h1
 
-        //console.log(siteHeading.html());
-        //console.log(siteHeading.text());
-        //const output = siteHeading
-        // .children('h1')
-        // .text();
+            //console.log(siteHeading.html()); // imprime o HTML do elemento h1
+            //console.log(siteHeading.text());  // imprime o texto do elemento h1
+            //const output = siteHeading  //
+            // .children('h1') // seleciona o elemento h1
+            // .text(); // imprime o texto do elemento h1
 
-        //  const output = siteHeading
-        //   .children('h2')
-        //   .parent()
-        //   .text();
-
-        $('#menu a').each((i, el) => {
-            const item = $(el).text();
-            const link = $(el).attr('href')
-
-            console.log(link);
-        });
+            //  const output = siteHeading
+            //   .children('h2')
+            //   .parent()
+            //   .text();
 
 
-    }
-});
+            $('#menu a').each((_, el) => { // seleciona todos os links dentro do menu 
+                const link = $(el).attr('href');  // pega o atributo href de cada link
+                console.log(link); // imprime o link
+            });
+        }
+    })
+    .catch(error => { // trata erros
+        console.error('Error fetching the URL:', error); // imprime um erro caso ocorra
+    });
