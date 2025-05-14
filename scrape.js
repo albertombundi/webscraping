efe048ff2b17fcf159c5dffbacb63d4a8f75427e
albertombundi <albertombundi@outlook.com>
2025-05-14 22:32:32 +0100

    Fiz uma extração de dados de um site qualquer

--- scrape.js
+++ scrape.js
@@ -1,10 +1,30 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-request('https://books.toscrape.com/', (error, response, html) => {
+request('https://fakestoreapi.com', (error, response, html) => {
     if(!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-
+        const $  = cheerio.load(html);
         
+        const siteHeading = $('.jumbotron');
+
+        //console.log(siteHeading.html());
+        //console.log(siteHeading.text());
+        //const output = siteHeading
+        // .children('h1')
+        // .text();
+
+        //  const output = siteHeading
+        //   .children('h2')
+        //   .parent()
+        //   .text();
+
+        $('#menu a').each((i, el) => {
+            const item = $(el).text();
+            const link = $(el).attr('href')
+
+            console.log(link);
+        });
+
+
     }
 });
